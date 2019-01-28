ba7e25d0483c186a40cd65eaf47b8a16590871f0
egfarr <farr.16@wright.edu>
2019-01-28 16:23:24 -0500

    Add main file

---
+++ HarborDrake/src/harbordrake/HarborDrake.java
@@ -0,0 +1,25 @@
+/*
+Welcome to the HarborDrake main project file
+This is a work in progress and you are free to contribute to it however you
+like.
+ */
+package harbordrake;
+
+/**
+ *
+ * @author Elizabeth Farr
+ */
+public class HarborDrake {
+
+    /**
+     * Main class
+     * TODO: change main file to a GUI
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Initial code to test that the files in the main git repository work
+        System.out.println("Hello World!");
+        //TODO: make everything
+    }
+    
+}
